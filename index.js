6c5c19fae6108edc50a28060b88501b9284ba3ed
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2023-12-22 20:36:20 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email,
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const newId = data.id;
+      const idElement = document.createElement("p");
+      idElement.textContent = newId;
+      document.body.appendChild(idElement);
+    })
+    .catch(error => {
+      console.log(error);
+      const errorMessage = error.message;
+      const errorElement = document.createElement("p");
+      errorElement.textContent = errorMessage;
+      document.body.appendChild(errorElement)
+    });
+}
+
+submitData("Steve", "steve@steve.com");
